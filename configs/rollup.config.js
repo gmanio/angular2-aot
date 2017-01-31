6300e2a53dbfa648e01bfb905af0aabe4ef1a6aa
gman.park <im@gman.io>
2017-01-31 17:49:22 +0900

    [TASK] build complete

--- configs/rollup.config.js
+++ configs/rollup.config.js
@@ -13,21 +13,25 @@ export default {
     dest: 'dist/build.js', // output a single application bundle
     sourceMap: false,
     format: 'iife',
-    onwarn: function(warning) {
+    onwarn: function (warning) {
         // Skip certain warnings
 
         // should intercept ... but doesn't in some rollup versions
-        if ( warning.code === 'THIS_IS_UNDEFINED' ) { return; }
+        if (warning.code === 'THIS_IS_UNDEFINED') {
+            return;
+        }
         // intercepts in some rollup versions
-        if ( warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1 ) { return; }
+        if (warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1) {
+            return;
+        }
 
         // console.warn everything else
-        console.warn( warning.message );
+        console.warn(warning.message);
     },
     plugins: [
         nodeResolve({jsnext: true, module: true}),
         commonjs({
-            include: '../node_modules/rxjs/**',
+            include: ['node_modules/rxjs/**', 'node_modules/core-js/**']
         }),
         uglify()
     ]
--- src/app/main-aot.ts
+++ src/app/main-aot.ts
@@ -3,7 +3,10 @@
  * @author: Gman Park
  */
 
-import { platformBrowser }    from '@angular/platform-browser';
-import { AppModuleNgFactory } from '../../aot/src/app/app.module.ngfactory';
-console.log('Running AOT compiled');
+import {platformBrowser}    from '@angular/platform-browser';
+import {AppModuleNgFactory} from '../../aot/src/app/app.module.ngfactory';
+import {enableProdMode} from "@angular/core";
+
+enableProdMode();
+
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
