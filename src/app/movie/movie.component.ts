3fd92bdadc6a6648b72ed2558e2be260902a067f
gman.park <im@gman.io>
2017-02-15 21:17:25 +0900

    [TASK] update proxy

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -54,7 +54,7 @@ export class MovieComponent {
 
   getMovieList() {
     // https://openapi.naver.com/v1/search/movie.json
-    return this.http.get('openapi.naver.com/v1/search/movie.json', {
+    return this.http.get('/v1/search/movie.json', {
       headers: new Headers({
         'Accept': '*/*',
         'X-Naver-Client-Id': MovieComponent.ClientId,
